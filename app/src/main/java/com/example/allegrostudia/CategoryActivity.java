47c28bfcba30e91764253709edb3534dffe8ca79
Dux97 <damianpiechaczek1@gmail.com>
2020-01-11 17:13:46 +0100

    dodanie strony kategorie z list view

--- app/src/main/java/com/example/allegrostudia/CategoryActivity.java
+++ app/src/main/java/com/example/allegrostudia/CategoryActivity.java
@@ -2,13 +2,34 @@ package com.example.allegrostudia;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 public class CategoryActivity extends AppCompatActivity {
 
-    @Override
+     String category[] = new String []{"Elektronika", "Dom"};
+
+     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_category);
+
+        ListView listView = (ListView) findViewById(R.id.listView);
+        ArrayAdapter<String> adapter= new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1 , category);
+        listView.setAdapter(adapter);
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                openCategoryActivity();
+            }
+        });
+    }
+    private void openCategoryActivity(){
+        Intent intent = new Intent(this,MainActivity.class);
+        startActivity(intent);
     }
 }
--- app/src/main/java/com/example/allegrostudia/StartActivity.java
+++ app/src/main/java/com/example/allegrostudia/StartActivity.java
@@ -9,35 +9,26 @@ import android.widget.Button;
 
 public class StartActivity extends AppCompatActivity {
 
-    private Button categorybtn;
+
     private Button searchbtn;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_start);
 
-        categorybtn = (Button) findViewById(R.id.Category);
-        categorybtn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                openCategoryActivity();
-            }});
 
         searchbtn = (Button) findViewById(R.id.Search);
         searchbtn.setOnClickListener(new View.OnClickListener() {
              @Override
              public void onClick(View v) {
-                 openCategoryActivity();
+                 openSearchActivity();
              }
 
          });
     }
-    public void openCategoryActivity() {
-        Intent intent = new Intent(this,MainActivity.class);
-        startActivity(intent);
-    }
+
     public void openSearchActivity() {
-        Intent intent = new Intent(this,MainActivity.class);
+        Intent intent = new Intent(this,CategoryActivity.class);
         startActivity(intent);
     }
 

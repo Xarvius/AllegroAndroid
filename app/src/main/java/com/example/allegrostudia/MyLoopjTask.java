c47a2d5a8d556ffb5f9518d9f7af414bf77f1a1f
Dux97 <damianpiechaczek1@gmail.com>
2020-01-11 13:32:37 +0100

    add jd

--- app/src/main/java/com/example/allegrostudia/MyLoopjTask.java
+++ app/src/main/java/com/example/allegrostudia/MyLoopjTask.java
@@ -18,6 +18,7 @@ public class MyLoopjTask {
     String BASE_URL = "https://api.allegro.pl/sale/categories/";
     String TOKEN = "eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCJ9.eyJzY29wZSI6WyJhbGxlZ3JvX2FwaSJdLCJleHAiOjE1Nzg3ODA2NjEsImp0aSI6ImM0MzViMWU5LWU0Y2YtNDA4OS1iNDU1LTgwMjIzOGM1YmQ1NyIsImNsaWVudF9pZCI6IjBkOGIyYzRhOTZjMzQ3Mjc5YTc5MTNmYThmOWI0YzNlIn0.gg6nS3Y0mkh0yXq68i44Plxj-HbLQk7NdkJTYn0MkyNEspe2GJQO3EnPAj4x6ddeIHf8XQtrP9KwlcBgtz4CQ1pIBWD4XEpzGMJS9H1dBx9rp2Ti3A4Fms_7612xpHqzwkR8V8iKpUrS_PKDNHNIsdG56cq9zvPmBNVRJSdReUeZYAr2lrBenB7IHm9UZevJo-JDw2ZA65cGK4mhELXIE7SoxpmNsnsMi21AE7tAg1L5SFBWP4RxfaRbOFqFJhhit7o_XlEOb4Q7juKn4n0WVUI3841eJKuRvDM4vulAy6x5zjXCOnGGXv93IcTN7EpkV7s_9cnenrIsXLd_DQvjNw";
     String jsonResponse;
+    String JD;
 
     public MyLoopjTask() {
         asyncHttpClient = new AsyncHttpClient();
